ed41160df2c275b72e31e88e7244179b3a646afc
Ali AL-Saeedi <alialsaeedi19@gmail.com>
2017-11-29 18:27:16 +0100

    adding all the npm packages dependencies commands

---
+++ downloading_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+npm install
+npm install http
+npm install express
+npm install --save request
+npm install --save request-promise
+npm install http --save
+npm install body-parser --save
